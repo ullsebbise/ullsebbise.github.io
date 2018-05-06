6bb9156909c85ab60610fb266ca2cd20ae02840b
Sebastian & Ulrika <ullsebbise@gmail.com>
2018-05-06 10:21:18 +0200

    karta tillagd

---
+++ script.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var lokal = {lat: 59.463671, lng: 17.919190};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: lokal
+  });
+  var marker = new google.maps.Marker({
+    position: lokal,
+    map: map
+  });
+}
